e4696397ab09a9b1caf0309d6f31602618cac75f
rlawjggns <rlawjggns12@gmail.com>
2024-05-09 20:44:14 +0900

    v0.2

--- iheww.py
+++ iheww.py
@@ -17,19 +17,20 @@ outfile.write(f"평균 일매출 = {sum/count}")
 
 infile.close()
 outfile.close()
-
-bar = int(input("나눠지는 수를 입력하세요 : "))
-foo = int(input("나누는 수를 입력하세요 : "))
+#################################3
 
 try :
-    divValue = bar/foo
+    bar = int(input("나눠지는 수를 입력하세요 : "))
+    foo = int(input("나누는 수를 입력하세요 : "))
+    result = bar/foo
+    print(f"{bar}를 {foo}로 나눈 결과는 {result}입니다.")
 except ValueError :
     print("정수를 입력해야 합니다.")
 except ZeroDivisionError :
     print("0으로 나눌 수 없습니다.")
-except :
-    print("오류가 발생했습니다.")
+except Exception as e:
+    print(f"오류가 발생했습니다. {e}")
 else :
-    print(f"나눈 결과 : {divValue}")
+    print("나누기 연산 성공!")
 finally :
     print("프로그램을 종료합니다")
